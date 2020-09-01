4408304504394545cb25e12241a8116d16e71511
Your Name <you@example.com>
2020-09-01 07:39:11 -0300

    Criado operadores.rb

---
+++ operadores.rb
@@ -0,0 +1,15 @@
+n1 = 0
+n2 = 0
+total = 0
+
+puts "Informe o n1: "
+n1 = gets.chomp.to_i
+
+puts "Informe o n2: "
+n2 = gets.chomp.to_i
+
+# 'to_i' transforma em inteiro
+
+total = n1 * n2
+
+puts "Total é " , total
---
+++ tipos.rb
@@ -0,0 +1,18 @@
+# Tipos...
+
+produto = 'Macbook'
+preco = 5999.99
+quantidade = 200
+disponivel = true
+
+puts produto.class
+puts preco.class
+puts quantidade.class
+puts disponivel.class
+
+puts "\n"
+
+puts produto
+puts preco
+puts quantidade
+puts disponivel
